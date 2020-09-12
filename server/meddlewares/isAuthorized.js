84b1b069a6b1b96b205cc2e6215fb39d53888ce5
Idrees <ahmadid@student.chalmers.se>
2020-09-12 17:44:31 +0200

    adding authentication meddleware

---
+++ server/meddlewares/isAuthorized.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  // the token should be sent as a header and it should start with "Bearer "
+  const headerWithToken = req.get('Authorization');
+  if (!headerWithToken) {
+    const error = new Error('User Not Authenticated');
+    err.statusCode = 401;
+    throw error;
+  }
+  const token = headerWithToken.split(' ')[1];
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(
+      token,
+      'thisIsSupposedToBeVeryLongForSecurityReasons'
+    );
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+  if (!decodedToken) {
+    const error = new Error('User Not Authenticated');
+    err.statusCode = 401;
+    throw error;
+  }
+  req.userId = headerWithToken.userId;
+  next();
+};
