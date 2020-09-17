b92008c7c4158ec33459336d5452d2bd6c94bd3a
SyncInProgress <bhavya@student.chalmers.se>
2020-09-17 13:09:13 +0200

    Separated recipes routes and controller\s

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,26 +1,23 @@
 
-const express = require('express');
-const router = express.Router();
-
 const Recipe = require('../models/recipe');
 
-router.post('/api/recipes', (req, res, next) => {
+exports.createRecipe = (req, res, next) => {
     const recipe = new Recipe(req.body);
     recipe.save((err, recipe) => {
         if (err) { return next(err); }
         res.status(201).json(recipe);
     });
-});
+};
 
-router.get('/api/recipes', (req, res, next) => {
+exports.getAllRecipes = (req, res, next) => {
     const sortBy = req.query.sort || '';
     Recipe.find((err, recipes) => {
         if (err) { return next(err); }
         res.json({'recipes': recipes });
     }).sort(sortBy)
-});
+};
 
-router.get('/api/recipes/:id', (req, res, next) => {
+exports.getRecipe = (req, res, next) => {
     const id = req.params.id;
     Recipe.findById(id, (err, recipe) => {
         if (err) { return next(err); }
@@ -29,9 +26,9 @@ router.get('/api/recipes/:id', (req, res, next) => {
         }
         res.json(recipe);
     });
-});
+};
 
-router.put('/api/recipes/:id', (req, res, next) => {
+exports.editRecipe = (req, res, next) => {
     const id = req.params.id;
     Recipe.findById(id, (err, recipe) => {
         if (err) { return next(err); }
@@ -40,15 +37,15 @@ router.put('/api/recipes/:id', (req, res, next) => {
         }
         recipe.name = req.body.name;
         recipe.ingredients = req.body.ingredients;
-        recipe.preperation = req.body.preperation;
+        recipe.preparation = req.body.preparation;
         recipe.cookingProcess = req.body.cookingProcess;
         recipe.toServe = req.body.toServe;
         recipe.save();
         res.json(recipe);
     });
-});
+};
 
-router.patch('/api/recipes/:id', (req, res, next) => {
+exports.updateRecipe = (req, res, next) => {
     const id = req.params.id;
     Recipe.findById(id, (err, recipe) => {
         if (err) { return next(err); }
@@ -57,15 +54,15 @@ router.patch('/api/recipes/:id', (req, res, next) => {
         }
         recipe.name = (req.body.name || recipe.name);
         recipe.ingredients = (req.body.ingredients || recipe.ingredients);
-        recipe.preperation = (req.body.preperation || recipe.preperation);
+        recipe.preparation = (req.body.preparation || recipe.preparation);
         recipe.cookingProcess = (req.body.cookingProcess || recipe.cookingProcess);
         recipe.toServe = (req.body.toServe || recipe.toServe);
         recipe.save();
         res.json(recipe);
     });
-});
+};
 
-router.delete('/api/recipes/:id', (req, res, next) => {
+exports.deleteRecipe = (req, res, next) => {
     const id = req.params.id;
     Recipe.findOneAndDelete({_id: id}, (err, recipe) => {
         if (err) { return next(err); }
@@ -74,6 +71,4 @@ router.delete('/api/recipes/:id', (req, res, next) => {
         }
         res.json(recipe);
     });
-});
-
-module.exports = router;
+};
---
+++ server/routes/recipe.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const recipeController = require('../controllers/recipes');
+
+const router = express.Router();
+
+router.post('/api/recipes', recipeController.createRecipe);
+router.get('/api/recipes', recipeController.getAllRecipes);
+router.get('/api/recipes/:id', recipeController.getRecipe);
+router.patch('/api/recipes/:id', recipeController.updateRecipe);
+router.put('/api/recipes/:id', recipeController.editRecipe);
+router.delete('/api/recipes/:id', recipeController.deleteRecipe);
+
+module.exports = router;
