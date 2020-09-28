dbe0625edc5931d747d885bf1d06b79a59699eab
Idrees <ahmadid@student.chalmers.se>
2020-09-28 13:04:48 +0200

    adding update buttons to profile

--- client/src/App.vue
+++ client/src/App.vue
@@ -18,8 +18,9 @@ export default {
 </script>
 
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Lora&family=Montserrat&display=swap");
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,42 +1,82 @@
 <template>
-  <div v-if="userInfo">
-    <div>
-      <h2 class="h2">Username:</h2>
-      <h4>{{ userInfo.username }}</h4>
-    </div>
-    <div>
-      <h2>Name:</h2>
-      <h4>{{ userInfo.name.firstName }} {{ userInfo.name.lastName }}</h4>
-    </div>
-    <div>
-      <h2>Email:</h2>
-      <h4>{{ userInfo.email }}</h4>
-    </div>
-    <div>
-      <h2>Country:</h2>
-      <h4>{{ userInfo.country }}</h4>
-    </div>
-    <div>
-      <h2>Bio:</h2>
-      <h4>{{ userInfo.bio }}</h4>
-    </div>
-    <div>
-      <h2>Your Role:</h2>
-      <h4>{{ userInfo.roleType }}</h4>
-    </div>
-    <div>
-      <h2>Your Posts:</h2>
-      <h4>your Posts</h4>
-    </div>
-    <div>
-      <h2>Your Comments:</h2>
-      <h4>your Comments</h4>
-    </div>
-    <div>
-      <h2>Post You Have Liked:</h2>
-      <h4>Post You Have Liked:</h4>
-    </div>
-  </div>
+  <b-container v-if="userInfo" class="profile-container">
+    <b-row offset="1">
+      <h1>Your Profile</h1>
+    </b-row>
+    <b-row cols="6">
+      <b-col  cols="6" offset="2">
+        <h2>Name:</h2>
+        <h4>{{ userInfo.name.firstName }} {{ userInfo.name.lastName }}</h4>
+      </b-col>
+      <b-col >
+        <h2 class="h2">Username:</h2>
+        <h4>{{ userInfo.username }}</h4>
+      </b-col>
+    </b-row>
+    <b-row >
+      <b-col cols="6" offset="2">
+        <h2>Email:</h2>
+        <h4>{{ userInfo.email }}</h4>
+      </b-col>
+      <b-col>
+        <h2>Country:</h2>
+        <h4>{{ userInfo.country }}</h4>
+      </b-col>
+    </b-row>
+    <b-row >
+      <b-col cols="6" offset="2">
+        <h2>Bio:</h2>
+        <h4>{{ userInfo.bio }}</h4>
+      </b-col>
+      <b-col>
+        <h2>Your Role:</h2>
+        <h4>{{ userInfo.roleType }}</h4>
+      </b-col>
+    </b-row>
+    <b-row offset="4">
+      <b-col cols="6" offset="2">
+        <b-button variant="outline-primary" class="mr-5 my-4"
+          >Update Profile</b-button
+        >
+        <b-button variant="outline-primary" class="ml-2 my-4"
+          >Update Password</b-button
+        >
+      </b-col>
+    </b-row>
+    <b-row class="mt-3">
+      <b-col >
+        <h2>Your Posts:</h2>
+        <b-col>your Posts</b-col>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-button variant="outline-primary" class="mr-5 my-4"
+        >Delete All My Post</b-button
+      >
+    </b-row>
+    <b-row class="mt-3">
+      <b-col >
+        <h2>Your Comments:</h2>
+        <div>your Comments</div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-button variant="outline-primary" class="mr-5 my-4"
+        >Delete All My comments</b-button
+      >
+    </b-row>
+    <b-row class="mt-3">
+      <b-col >
+        <h2>Post You Have Liked:</h2>
+        <div>Post You Have Liked:</div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-button variant="outline-primary" class="mr-5 my-4"
+        >Remore My Likes</b-button
+      >
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -45,7 +85,9 @@
 export default {
   computed: {
     userInfo() {
-      return !this.$store.getters.userInfo ? false : this.$store.getters.userInfo
+      return !this.$store.getters.userInfo
+        ? false
+        : this.$store.getters.userInfo
     }
   },
   created() {
@@ -53,3 +95,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.profile-container {
+  margin-top: 2rem;
+}
+
+</style>
