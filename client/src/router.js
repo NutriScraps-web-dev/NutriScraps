54b5dc3e9bd12c589323d6d786ad8b05c88d6dec
SyncInProgress <bhavya@student.chalmers.se>
2020-10-02 00:28:18 +0200

    Added Recipe Frontend

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import ContactUs from './views/ContactUs.vue'
+import Recipes from './views/Recipes.vue'
 
 Vue.use(Router)
 
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/contact-us',
+      name: 'contactUs',
+      component: ContactUs
+    },
+    {
+      path: '/recipes',
+      name: 'recipes',
+      component: Recipes
     }
   ]
 })
---
+++ client/src/views/ContactUs.vue
@@ -0,0 +1,105 @@
+<template>
+  <b-container class="contact-container">
+    <div>
+      <h3>Contact Us</h3>
+      <br>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group
+          id="input-group-1"
+          label="Email address:"
+          label-for="input-1"
+          class="email"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Your Name:" label-for="input-2" class="name">
+          <b-form-input
+            id="input-2"
+            v-model="form.name"
+            required
+            placeholder="Enter name"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-3" label="Message:" label-for="imput-3" class="message">
+          <b-form-textarea
+            id="textarea"
+            v-model="form.message"
+            required
+            placeholder="Enter something..."
+            rows="3"
+            max-rows="6"
+          ></b-form-textarea>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+      <b-card class="mt-3" header="Form Data Result">
+        <pre class="m-0">{{ form }}</pre>
+      </b-card>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        name: '',
+        message: ''
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(evt) {
+      evt.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.message = ''
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.contact-container {
+  margin: auto;
+  width: 50%;
+  padding: 20px;
+}
+
+.name{
+  text-align: left;
+}
+
+.email{
+  text-align: left;
+}
+
+.message{
+  text-align: left;
+}
+
+</style>
