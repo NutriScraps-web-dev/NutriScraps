4ed15ff660db47a1b70d161556115d19e927058b
SyncInProgress <bhavya@student.chalmers.se>
2020-10-17 13:12:16 +0200

    Routes updated

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const recipeController = require('../controllers/recipes');
+const isAuth = require('../middleware/isAuthorized');
 
 const router = express.Router();
 
-router.post('/api/recipes', recipeController.createRecipe);
+router.post('/api/recipes', isAuth, recipeController.createRecipe);
 router.get('/api/recipes', recipeController.getAllRecipes);
 router.get('/api/recipes/:id', recipeController.getRecipe);
-router.patch('/api/recipes/:id', recipeController.updateRecipe);
-router.put('/api/recipes/:id', recipeController.editRecipe);
-router.delete('/api/recipes/:id', recipeController.deleteRecipe);
-router.get('/api/external/recipes', recipeController.getRecipeByIngredients);
-router.get('/api/external/recipes', recipeController.getRecipeComplexQuery);
+router.patch('/api/recipes/:id', isAuth, recipeController.updateRecipe);
+router.put('/api/recipes/:id', isAuth, recipeController.editRecipe);
+router.delete('/api/recipes/:id', isAuth, recipeController.deleteRecipe);
+//router.get('/api/search', recipeController.getRecipeFromAPI);
 
 
 module.exports = router;
